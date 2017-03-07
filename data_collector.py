c5a0e223489c83ba8eca5554c81303fd6fb2f468
tuwid <tuwi.dc@gmail.com>
2017-03-07 22:12:38 +0100

    minor buggix on outerNIC

--- data_collector.py
+++ data_collector.py
@@ -100,17 +100,7 @@ def check_disks():
 
 
 def check_outer_nic():
-    route = '/proc/net/route'
-    with open(route) as f:
-            for line in f.readlines():
-                    try:
-                            iface, dest, _, flags, _, _, _, _, _, _, _, =  line.strip().split()
-                            if dest != '00000000' or not int(flags, 16) & 2:
-                                    continue
-                            return iface
-                    except:
-                            continue
-
+    return subprocess.Popen("ip route | grep '^default' |  awk '{ print $5 }'",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate()[0].rstrip()
 
 def post_to_api(data):
     post_data = {
